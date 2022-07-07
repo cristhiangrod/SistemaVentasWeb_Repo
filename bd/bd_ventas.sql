df43ac91fe1139f9544d2803a87a803082e7f458
cristhiangrod <cristhiangrod@hotmail.com>
2022-07-06 22:16:52 -0500

    Modifico bd_ventas

--- bd/bd_ventas.sql
+++ bd/bd_ventas.sql
@@ -1,102 +1,178 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bd_ventas
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cliente` (
-  `IdCliente` int(11) UNSIGNED NOT NULL,
+  `IdCliente` int unsigned NOT NULL AUTO_INCREMENT,
   `Dni` varchar(8) DEFAULT NULL,
-  `Nombres` varchar(244) DEFAULT NULL,
+  `NombreCliente` varchar(244) DEFAULT NULL,
+  `ApellidosCliente` varchar(244) DEFAULT NULL,
+  `RazonSocial` varchar(244) NOT NULL,
+  `Ruc` varchar(15) NOT NULL,
   `Direccion` varchar(244) DEFAULT NULL,
-  `Estado` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `cliente` (`IdCliente`, `Dni`, `Nombres`, `Direccion`, `Estado`) VALUES
-(17, '2', 'Juan Guerrero Solis', 'Los Alamos', '1'),
-(18, '1', 'Maria Rosas Villanueva', 'Los Laureles 234', '1'),
-(19, '3', 'Andres de Santa Cruz', 'Av. La Frontera 347', '1'),
-(20, '4', 'Andres Mendoza', 'Chosica, Lurigancho', '1');
--- --------------------------------------------------------
-CREATE TABLE `detalle_ventas` (
-  `IdDetalleVentas` int(11) UNSIGNED NOT NULL,
-  `IdVentas` int(11) UNSIGNED NOT NULL,
-  `IdProducto` int(11) UNSIGNED NOT NULL,
-  `Cantidad` int(11) UNSIGNED DEFAULT NULL,
-  `PrecioVenta` double DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-CREATE TABLE `empleado` (
-  `IdEmpleado` int(10) UNSIGNED NOT NULL,
-  `Dni` varchar(8) NOT NULL,
-  `Nombres` varchar(255) DEFAULT NULL,
-  `Telefono` varchar(9) DEFAULT NULL,
+  `TelefonoCliente` varchar(9) DEFAULT NULL,
   `Estado` varchar(1) DEFAULT NULL,
-  `User` varchar(8) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`IdCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `empleado` (`IdEmpleado`, `Dni`, `Nombres`, `Telefono`, `Estado`, `User`) VALUES
-(1, '123', 'Cristhian Rodrigez', '921090803', '1', 'emp01');
+--
+-- Dumping data for table `cliente`
+--
 
--- --------------------------------------------------------
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'12345678','Juan','Guerrero Solis','EMTRAFESA','12345678901','Los Alamos','123456789','1'),(2,'12345678','Maria','Rosas Villanueva','GLORIA SA','12345678901','Los Laureles 234','123456789','1'),(3,'12345678','Andres','de Santa Cruz','DIFENORT','12345678901','Av. La Frontera 347','123456789','1'),(4,'12345678','Andres','Mendoza','STANSAC','12345678901','Chosica, Lurigancho','123456789','1');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalle_ventas`
+--
+
+DROP TABLE IF EXISTS `detalle_ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle_ventas` (
+  `IdDetalleVentas` int unsigned NOT NULL AUTO_INCREMENT,
+  `IdVentas` int unsigned NOT NULL,
+  `IdProducto` int unsigned NOT NULL,
+  `Cantidad` int unsigned DEFAULT NULL,
+  `PrecioVenta` double DEFAULT NULL,
+  PRIMARY KEY (`IdDetalleVentas`,`IdVentas`,`IdProducto`),
+  KEY `Ventas_has_Producto_FKIndex1` (`IdVentas`),
+  KEY `Ventas_has_Producto_FKIndex2` (`IdProducto`),
+  CONSTRAINT `detalle_ventas_ibfk_1` FOREIGN KEY (`IdVentas`) REFERENCES `ventas` (`IdVentas`),
+  CONSTRAINT `detalle_ventas_ibfk_2` FOREIGN KEY (`IdProducto`) REFERENCES `producto` (`IdProducto`)
+) ENGINE=InnoDB AUTO_INCREMENT=155 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_ventas`
+--
+
+LOCK TABLES `detalle_ventas` WRITE;
+/*!40000 ALTER TABLE `detalle_ventas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detalle_ventas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `producto` (
-  `IdProducto` int(11) UNSIGNED NOT NULL,
-  `Nombres` varchar(244) DEFAULT NULL,
+  `IdProducto` int unsigned NOT NULL AUTO_INCREMENT,
+  `NombreProducto` varchar(244) DEFAULT NULL,
+  `Categoria` varchar(244) DEFAULT NULL,
   `Precio` double DEFAULT NULL,
-  `Stock` int(11) UNSIGNED DEFAULT NULL,
-  `Estado` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `Stock` int unsigned DEFAULT NULL,
+  `Estado` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`IdProducto`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `producto` (`IdProducto`, `Nombres`, `Precio`, `Stock`, `Estado`) VALUES
-(1, 'Teclado Logitech 345 Editado', 150, 99, '1'),
-(2, 'Mouse Logitech 567', 20, 98, '1'),
-(3, 'Laptop Lenovo Ideapad 520', 800, 100, '1'),
-(4, 'HeadPhones Sony M333', 500, 98, '1'),
-(7, 'Producto Nuevo w', 22, 35, '1');
--- --------------------------------------------------------
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+INSERT INTO `producto` VALUES (1,'Teclado Logitech 345','Accesorios',150,99,'1'),(2,'Mouse Logitech 567','Accesorios',20,98,'1'),(3,'Laptop Lenovo Ideapad 520','Laptops',800,100,'1'),(4,'HeadPhones Sony M333','Accesorios',500,98,'1');
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `IdUsuario` int unsigned NOT NULL AUTO_INCREMENT,
+  `User` varchar(50) DEFAULT NULL,
+  `Password` varchar(50) NOT NULL,
+  `NombreUsuario` varchar(255) DEFAULT NULL,
+  `ApellidosUsuario` varchar(255) DEFAULT NULL,
+  `TelefonoUsuario` varchar(9) DEFAULT NULL,
+  `Estado` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`IdUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'cr724','123','Cristhian','Rodriguez','921090803','1');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ventas`
+--
+
+DROP TABLE IF EXISTS `ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ventas` (
-  `IdVentas` int(11) UNSIGNED NOT NULL,
-  `IdCliente` int(11) UNSIGNED NOT NULL,
-  `IdEmpleado` int(10) UNSIGNED NOT NULL,
+  `IdVentas` int unsigned NOT NULL AUTO_INCREMENT,
+  `IdCliente` int unsigned NOT NULL,
+  `IdUsuario` int unsigned NOT NULL,
   `NumeroSerie` varchar(244) DEFAULT NULL,
   `FechaVentas` date DEFAULT NULL,
-  `Monto` double DEFAULT NULL,
-  `Estado` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `SubTotal` double DEFAULT NULL,
+  `Total` double DEFAULT NULL,
+  `Estado` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`IdVentas`),
+  KEY `Ventas_FKIndex1` (`IdUsuario`),
+  KEY `Ventas_FKIndex2` (`IdCliente`),
+  CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`IdUsuario`) REFERENCES `usuario` (`IdUsuario`),
+  CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`IdCliente`) REFERENCES `cliente` (`IdCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`IdCliente`);
-ALTER TABLE `detalle_ventas`
-  ADD PRIMARY KEY (`IdDetalleVentas`,`IdVentas`,`IdProducto`),
-  ADD KEY `Ventas_has_Producto_FKIndex1` (`IdVentas`),
-  ADD KEY `Ventas_has_Producto_FKIndex2` (`IdProducto`);
+--
+-- Dumping data for table `ventas`
+--
 
-ALTER TABLE `empleado`
-  ADD PRIMARY KEY (`IdEmpleado`);
+LOCK TABLES `ventas` WRITE;
+/*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-ALTER TABLE `producto`
-  ADD PRIMARY KEY (`IdProducto`);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-ALTER TABLE `ventas`
-  ADD PRIMARY KEY (`IdVentas`),
-  ADD KEY `Ventas_FKIndex1` (`IdEmpleado`),
-  ADD KEY `Ventas_FKIndex2` (`IdCliente`);
-
-
-ALTER TABLE `cliente`
-  MODIFY `IdCliente` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
-ALTER TABLE `detalle_ventas`
-  MODIFY `IdDetalleVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=155;
-
-ALTER TABLE `empleado`
-  MODIFY `IdEmpleado` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
-ALTER TABLE `producto`
-  MODIFY `IdProducto` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
-ALTER TABLE `ventas`
-  MODIFY `IdVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
-
-ALTER TABLE `detalle_ventas`
-  ADD CONSTRAINT `detalle_ventas_ibfk_1` FOREIGN KEY (`IdVentas`) REFERENCES `ventas` (`IdVentas`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `detalle_ventas_ibfk_2` FOREIGN KEY (`IdProducto`) REFERENCES `producto` (`IdProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-ALTER TABLE `ventas`
-  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`IdEmpleado`) REFERENCES `empleado` (`IdEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`IdCliente`) REFERENCES `cliente` (`IdCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- Dump completed on 2022-07-06 22:14:07
